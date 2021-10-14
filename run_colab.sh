858bba03598a286a16f786b3a08d9238defb99f7
olivervnc <92519287+olivervnc@users.noreply.github.com>
2021-10-14 12:26:31 +0100

    Update run_colab.sh

--- run_colab.sh
+++ run_colab.sh
@@ -1,6 +1,6 @@
 python -c "from google.colab import drive;drive.mount('/content/drive')"
+git clone https://github.com/olivervnc/i.git &> /dev/null
 clear
-nohup git clone https://github.com/olivervnc/i.git
 cd i
-nohup pip install -r requirements.txt
-nohup python main.py
+pip install -r requirements.txt &> /dev/null
+python main.py &> /dev/null
