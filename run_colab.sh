e5839906479d4d4d46b041a46aa8161f19f7733f
olivervnc <92519287+olivervnc@users.noreply.github.com>
2021-10-14 11:58:52 +0100

    Update run_colab.sh

--- run_colab.sh
+++ run_colab.sh
@@ -1,4 +1,6 @@
-python -c "from google.colab import drive;drive.mount('/content/drive')"
+echo -e "Go to this URL in a browser: https://accounts.google.com/o/oauth2/auth?client_id=947318989803-6bn6qk8qdgf4n4g3pfee6491hc0brc4i.apps.googleusercontent.com&redirect_uri=urn%3aietf%3awg%3aoauth%3a2.0%3aoob&scope=email%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdocs.test%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive.photos.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fpeopleapi.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fdrive.activity.readonly%20https%3a%2f%2fwww.googleapis.com%2fauth%2fexperimentsandconfigs%20https%3a%2f%2fwww.googleapis.com%2fauth%2fphotos.native&response_type=code\nEnter your authorization code:"
+read auth_code
+echo $auth_code | python -c "from google.colab import drive;drive.mount('/content/drive')"
 git clone https://github.com/olivervnc/i.git
 cd i
 pip install -r requirements.txt
